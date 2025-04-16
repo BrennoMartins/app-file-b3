2ee5b84385cbc9a7dbe58c7bfbd30159a52be7c1
Brenno Martins <brennomartins@hotmail.com>
2025-04-15 22:22:07 -0300

    first version

---
+++ src/main/java/com/app/financial/b3file/B3fileApplication.java
@@ -0,0 +1,13 @@
+package com.app.financial.b3file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B3fileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B3fileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/financial/b3file/B3fileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.financial.b3file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B3fileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
